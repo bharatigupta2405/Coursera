82d3e9b3973db80a372ff32e95a8128d90d5ca8f
unknown <bharatigupta2405@gmail.com>
2020-09-23 20:22:35 +0530

    adding two numbers

--- Sum.py
+++ Sum.py
@@ -1,3 +1,5 @@
-a=int(input())
-b=int(input())
-print(a+b)
+def sum_of_digits(first_digit, second_digit):
+return first_digit + second_digit
+if __name__ == ’__main__’:
+a, b = map(int, input().split())
+print(sum_of_digits(a, b))
